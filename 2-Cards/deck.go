871b0e17d63597b5822de7d63b030b1711d4b135
Victor Gwee <47884004+vicgwee@users.noreply.github.com>
2022-02-03 17:19:00 +0800

    reading and writing from files with ioutils

--- 2-Cards/deck.go
+++ 2-Cards/deck.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -33,4 +35,19 @@ func (d deck) print() {
 
 func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
 }
--- 2-Cards/main.go
+++ 2-Cards/main.go
@@ -1,10 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := newDeck()
-	fmt.Println(cards.toString())
+	cards.saveToFile("cards.txt")
+	cards = newDeckFromFile("cards.txt")
+	cards.print()
 }
 
 func newCard() string {
