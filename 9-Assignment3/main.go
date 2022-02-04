844045494e01c18ac40fafd158fd568572235ba8
Victor Gwee <47884004+vicgwee@users.noreply.github.com>
2022-02-04 12:24:09 +0800

    pass myfile.txt as an argument

--- 9-Assignment3/main.go
+++ 9-Assignment3/main.go
@@ -7,7 +7,12 @@ import (
 )
 
 func main() {
-	file, err := os.Open("myfile.txt")
+	if len(os.Args) != 2 {
+		fmt.Println("Error: number of arguments provided: ", len(os.Args)-1)
+		os.Exit(1)
+	}
+	fname := os.Args[1]
+	file, err := os.Open(fname)
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
